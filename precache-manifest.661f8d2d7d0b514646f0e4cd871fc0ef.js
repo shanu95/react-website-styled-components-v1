5a7357d3c3a3f7b8f7032edef47ef36637750fcc
Shan Nirmala <daredevil@Shans-MacBook-Air.local>
2021-08-11 12:38:42 +0530

    Updates

---
+++ precache-manifest.661f8d2d7d0b514646f0e4cd871fc0ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c01e91663a3be20220182746850e3e",
+    "url": "/react-website-styled-components-v1/index.html"
+  },
+  {
+    "revision": "dc2a8b721c7810bcab3a",
+    "url": "/react-website-styled-components-v1/static/js/2.5f3ce4cc.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/react-website-styled-components-v1/static/js/2.5f3ce4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb3ca9823dea55a70e5",
+    "url": "/react-website-styled-components-v1/static/js/main.00f89741.chunk.js"
+  },
+  {
+    "revision": "84340809d95ffa9a9416",
+    "url": "/react-website-styled-components-v1/static/js/runtime-main.5fec2512.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/react-website-styled-components-v1/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/react-website-styled-components-v1/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/react-website-styled-components-v1/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/react-website-styled-components-v1/static/media/svg-3.397c455a.svg"
+  }
+]);
